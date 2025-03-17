b0df207fa972709ee6a1418e7ef7e634e4d86b53
Soumen Trivedi <soumen.trivedi@gmail.com>
2025-03-17 23:46:23 +0000

    fixed routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const routes = require("./src/routes");
+const routes = require("./routes");
 
 const app = express();
 app.use(cors());
